9b219c4ded17a9ead60c3f8dc3f3096a63eea01b
Philippe Martinet <phi.mar.email@gmail.com>
2018-06-11 21:56:03 +0200

    Performed two run with Adapted and Binarized algoritms with an accuracy of 15% and 13% respectively

--- model.py
+++ model.py
@@ -46,7 +46,7 @@ def main():
 
 
         from skmultilearn.adapt import MLkNN
-        classifier = MLkNN(k=20)
+        classifier = MLkNN(k=4)
 
         print("Train Adapted algorithm")
 
@@ -58,14 +58,16 @@ def main():
         from sklearn.metrics import accuracy_score
 
         print("Accuracy")
+        print(y_test.shape,predictions.shape)
         print(accuracy_score(y_test.toarray(),predictions))
 
-    #adapt(X_train,y_train,X_test,y_test)
+    adapt(X_train,y_train,X_test,y_test)
 
     def naive(X_train,y_train,X_test,y_test):
 
         y_train = y_train.values   # DataFrame to numpy array
-        y_test = y_test.values
+        #y_test = y_test.values
+        #y_test = y_test.to_coo()
         
         #from sklearn.naive_bayes import GaussianNB
         from sklearn.svm import SVC
@@ -76,15 +78,20 @@ def main():
         #clf = GaussianNB()
         classif = OneVsRestClassifier(estimator=SVC(random_state=0))
         print('y_train', y_train.shape)
-        y_train = MultiLabelBinarizer().fit_transform(y_train)
+        #y_train = MultiLabelBinarizer().fit_transform(y_train)
         print('y_train', y_train.shape)
         print(2)
         clf= classif.fit(X_train, y_train)
         print('X_test', X_test.shape)
-        predictions =clf.predict(X_test.toarray())
+
+
+        print("Predict")
+        
+        predictions = clf.predict(X_test)
         print(3)
         
         from sklearn.metrics import accuracy_score
+
 
         # for i in range(0,21):
         #     print("Train category {}".format(i))
@@ -100,29 +107,67 @@ def main():
 
         def test():
             X = [[1, 2], [2, 4], [4, 5], [3, 2], [3, 1]]
-            y = [[0, 1], [0, 2], [1, 3], [0, 2, 3], [2, 4]]
+            y = [[0, 1], [0, 2], [1, 3], [2, 3], [2, 4]]
             print(y.shape)
             y = MultiLabelBinarizer().fit_transform(y)
             print(y.shape)
             print(classif.fit(X, y).predict(X))
         
-        test()
+        #test()
         
         print("y and pred shapse {} {}".format(y_test.shape,predictions.shape))
-        print('y', y_test)
-        print('pred',dir( predictions))
-        print(predictions.view())
+        #print('y', y_test)
+        #print('pred',dir( predictions))
+        #print(predictions.view())
+        print(y_test.shape,predictions.shape)
         print(accuracy_score(y_test,predictions))
 
 
 
     naive(X_train,y_train,X_test,y_test)
     
-    print("End")
+   
     
 
 if __name__ == '__main__':
     print("Started ...")
     main()
+    print("End")
+
+'''
+Results: 180611
+
+Started ...
+# Import Data for adapted alogrithm
+X and y trainning data
+(21519, 2)
+(21519, 22)
+X and y Testing data
+(7077, 2)
+(7077, 22)
+Vectorize and normalize
+X_train: (21519, 25539)
+X_test (7077, 25539)
+Train Adapted algorithm
+Predict
+Accuracy
+(7077, 22) (7077, 22)
+0.1520418256323301
+MultiLabelBinarized
+y_train (21519, 22)
+y_train (21519, 22)
+2
+X_test (7077, 25539)
+Predict
+3
+y and pred shapse (7077, 22) (7077, 22)
+(7077, 22) (7077, 22)
+0.13183552352691819
+End
+
+real	44m6.005s
+user	43m21.202s
+sys	0m32.208s
 
 
+'''
